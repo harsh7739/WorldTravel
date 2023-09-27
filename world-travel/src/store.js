cdd80811e766edcf6b3e453fab072de429887b08
maniRhombuz <manish@rhombuz.io>
2023-09-27 21:32:35 +0530

    destination

--- world-travel/src/store.js
+++
@@ -1,9 +0,0 @@
-import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
-import { reducer as destinationReducer } from "./Destination Page/redux/reducer";
-import thunk from 'redux-thunk'
-
-const rootReducer = combineReducers({
-    destinationReducer
-})
-
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
